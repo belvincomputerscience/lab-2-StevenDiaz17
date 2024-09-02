bb01ca902c7b63fe74d5caeeb09dbee7edb06f0a
StevenDiaz17 <chasedcorgie@gmail.com>
2024-09-02 04:32:52 +0000

    I made this code to calculate the average test grades. (Made by Steven Diaz for Lab 2)

--- lab2.cpp
+++ lab2.cpp
@@ -1,1 +1,23 @@
+// This program was made by Steven Diaz for the Lab 2 assignment
+#include <iostream>
+using namespace std;
+int main()
+{
+    int grade1;
+    grade1 = 86;
+    int grade2;
+    grade2 = 75;
+    int grade3;
+    grade3 = 62;
+    int grade4;
+    grade4 = 98;
+    int semiresult;
+    int result;
 
+    semiresult = grade1 * grade2 * grade3 * grade4;
+    result = semiresult/4
+
+    cout << "The average grade is" << result << endl;
+
+    return 0;
+}
